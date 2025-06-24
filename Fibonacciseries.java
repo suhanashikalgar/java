3be04eeaf1191d5ffc52692b6aa92135b6df34bc
suhanashikalgar <suhanashikalgar024@gmail.com>
2025-06-24 12:42:19 +0530

    Create Fibonacciseries.java

---
+++ Fibonacciseries.java
@@ -0,0 +1,27 @@
+package com.dkte;
+
+public class Fibonacciseries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+	int n=6;
+	int i = 0;
+	int j=1;
+	int nt;
+	
+    System.out.println("Fibonacci Series:");
+    for(i=0 ; i<n ; i++)
+    {
+    	nt=i+j;
+    	i=j;
+    	j=nt;
+    	
+    	System.out.println(nt);
+    }
+        
+        
+	}
+
+}
