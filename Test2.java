db9b8245c215c407a8c01cb587c739a99811c71b
suhanashikalgar <suhanashikalgar024@gmail.com>
2025-06-25 13:04:08 +0530

    Create Test2.java

---
+++ Test2.java
@@ -0,0 +1,29 @@
+package com.dkte;
+import java.util.Scanner;
+
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner sc = new Scanner(System.in);
+     
+       System.out.println("Enter 1st no. :");
+       if(!sc.hasNextDouble()) {
+    	   
+    	  System.out.println("error");
+       }
+       else
+       {
+    	
+           double n1 =sc.nextDouble();
+           System.out.println("Enter 2nd no.");
+           double n2 =sc.nextDouble();
+           
+ double avg = n1+n2/2;
+    	   
+    	   System.out.println("Average:"+avg);
+       }
+	}
+
+}
