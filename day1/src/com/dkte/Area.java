5a6bb95aea81397d5fc8b937df41888a1cf5de8a
suhanashikalgar <suhanashikalgar024@gmail.com>
2025-07-02 11:59:25 +0530

    Add files via upload

---
+++ day1/src/com/dkte/Area.java
@@ -0,0 +1,18 @@
+package com.dkte;
+
+public class Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int length =4;
+int width = 5;
+int area;
+
+area = length * width;
+
+System.out.println("Length is :"+length);
+System.out.println("Width is :"+width);
+System.out.println("Area is :"+area);
+	}
+
+}
---
+++ day1/src/com/dkte/collegestudent.java
@@ -0,0 +1,21 @@
+package com.dkte;
+
+public class collegestudent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String studentName = "John Doe";
+		int studentID = 15;
+		int studentAge = 23;
+		float studentFee = 75.25f;
+		char studentGrade = 'B';
+
+		// Print variables
+		System.out.println("Student name: " + studentName);
+		System.out.println("Student id: " + studentID);
+		System.out.println("Student age: " + studentAge);
+		System.out.println("Student fee: " + studentFee);
+		System.out.println("Student grade: " + studentGrade);
+	}
+
+}
