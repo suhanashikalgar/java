ca35810c823281ca06cc72f667ba6361e81ed260
suhanashikalgar <suhanashikalgar024@gmail.com>
2025-07-02 12:08:55 +0530

    Add files via upload

---
+++ java/Arithmetic.java
@@ -0,0 +1,12 @@
+package math;
+
+public class Arithmetic{
+public void add(int a,int b){
+ int r=a+b;
+System.out.println(r);
+}
+public void substract(int a,int b){
+int r = a-b;
+System.out.println(r);
+}
+}
---
+++ java/Program.java
@@ -0,0 +1,10 @@
+package main;
+import math.Arithmetic;
+
+class Program {
+     public static void main(String[] args) {
+         Arithmetic a = new Arithmetic();
+		a.add(4 ,5);
+                a.substract(7 ,3);
+	}
+}
